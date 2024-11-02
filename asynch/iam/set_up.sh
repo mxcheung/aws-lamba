6511a2fe1910a4813438ed38526ffc3ed7471865
mxcheung <mxcheung@users.noreply.github.com>
2024-11-02 17:53:09 +1100

    Update set_up.sh

--- asynch/iam/set_up.sh
+++ asynch/iam/set_up.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-export LAMBDA_FUNCTION_NAME="S3TriggerLambda"
-export LAMBDA_ROLE_NAME="LambdaS3ExecutionRole"
+export LAMBDA_FUNCTION_1_NAME="LambdaFunction1"
+export LAMBDA_FUNCTION_2_NAME="LambdaFunction2"
+export LAMBDA_1_ROLE_NAME="LambdaInvokeRole1"
+export LAMBDA_2_ROLE_NAME="LambdaInvokeRole2"
 
 aws iam create-role \
-    --role-name  $LAMBDA_ROLE_NAME \
+    --role-name  $LAMBDA_1_ROLE_NAME \
     --assume-role-policy-document file://trust-policy.json
 
-# Wait for the IAM role to be created
-aws iam wait role-exists --role-name $LAMBDA_ROLE_NAME
-
-
-aws iam attach-role-policy --role-name $LAMBDA_ROLE_NAME --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
-aws iam attach-role-policy --role-name $LAMBDA_ROLE_NAME --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
+aws iam create-role \
+    --role-name  $LAMBDA_2_ROLE_NAME \
+    --assume-role-policy-document file://trust-policy.json
     
+# Wait for the IAM role to be created
+aws iam wait role-exists --role-name $LAMBDA_1_ROLE_NAME
+aws iam wait role-exists --role-name $LAMBDA_2_ROLE_NAME
+
+
+aws iam attach-role-policy --role-name $LAMBDA_1_ROLE_NAME --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
+aws iam attach-role-policy --role-name $LAMBDA_2_ROLE_NAME --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
