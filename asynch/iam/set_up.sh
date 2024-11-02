146301a8ed3cc7ab162c01395c7b7fa4305e1bc5
mxcheung <mxcheung@users.noreply.github.com>
2024-11-02 21:12:40 +1100

    Update set_up.sh

--- asynch/iam/set_up.sh
+++ asynch/iam/set_up.sh
@@ -20,3 +20,15 @@ aws iam wait role-exists --role-name $LAMBDA_2_ROLE_NAME
 
 aws iam attach-role-policy --role-name $LAMBDA_1_ROLE_NAME --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
 aws iam attach-role-policy --role-name $LAMBDA_2_ROLE_NAME --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
+
+
+chmod +x generate_invoke_policy.sh
+
+./generate_invoke_policy.sh
+
+aws iam put-role-policy \
+    --role-name LambdaInvokeRole1 \
+    --policy-name InvokeLambdaFunction2Policy \
+    --policy-document file://invoke_policy.json
+
+    
