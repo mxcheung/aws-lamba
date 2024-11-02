d2f6b7e44b3e8829b7160ede9ec1ff21a004da2a
mxcheung <mxcheung@users.noreply.github.com>
2024-11-02 17:48:21 +1100

    Update set_up.sh

--- asynch/set_up.sh
+++ asynch/set_up.sh
@@ -6,21 +6,10 @@ cd /home/ec2-user/aws-s3/presign_url/user_credentials
 cd /home/ec2-user/aws-s3/presign_url/iam
 . ./set_up.sh
 
-cd /home/ec2-user/aws-s3/presign_url/s3
+cd /home/ec2-user/aws-s3/presign_url/lambda_1
 . ./set_up.sh
 
-cd /home/ec2-user/aws-s3/presign_url/lambda
-. ./set_up.sh
-
-cd /home/ec2-user/aws-s3/presign_url/lambda_presign_url
+cd /home/ec2-user/aws-s3/presign_url/lambda_2
 . ./set_up.sh
 
 
-cd /home/ec2-user/aws-s3/presign_url/lambda_permission
-. ./set_up.sh
-
-cd /home/ec2-user/aws-s3/presign_url/s3_notification
-. ./set_up.sh
-
-cd /home/ec2-user/aws-s3/presign_url/s3_test_upload
-. ./set_up.sh
