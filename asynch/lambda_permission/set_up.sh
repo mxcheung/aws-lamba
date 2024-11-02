dbb215a4dbe1a1ace74023b9b29e55457507af82
mxcheung <mxcheung@users.noreply.github.com>
2024-11-02 17:58:32 +1100

    Update set_up.sh

--- asynch/lambda_permission/set_up.sh
+++ asynch/lambda_permission/set_up.sh
@@ -6,6 +6,9 @@ export LAMBDA_FUNCTION_2_NAME="LambdaFunction2"
 export LAMBDA_1_ROLE_NAME="LambdaInvokeRole1"
 export LAMBDA_2_ROLE_NAME="LambdaInvokeRole2"
 
+REGION="us-east-1"                                  # Replace with your AWS region
+
+
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
 
 
@@ -13,4 +16,4 @@ aws lambda add-permission --function-name $LAMBDA_FUNCTION_2_NAME \
     --statement-id AllowInvokeFromLambdaFunction1 \
     --action lambda:InvokeFunction \
     --principal lambda.amazonaws.com \
-    --source-arn arn:aws:lambda:<region>:$AWS_ACCOUNT_ID:function:$LAMBDA_FUNCTION_1_NAME
+    --source-arn arn:aws:lambda:$REGION:$AWS_ACCOUNT_ID:function:$LAMBDA_FUNCTION_1_NAME
