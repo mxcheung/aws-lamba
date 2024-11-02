ac335b5312f052434a27e635e89326208d11e0ad
mxcheung <mxcheung@users.noreply.github.com>
2024-11-02 20:46:19 +1100

    Update set_up.sh

--- asynch/lambda_1/set_up.sh
+++ asynch/lambda_1/set_up.sh
@@ -16,11 +16,11 @@ aws iam wait role-exists --role-name $LAMBDA_ROLE_NAME
 
 export LAMBDA_ROLE_ARN=$(aws iam get-role --role-name $LAMBDA_ROLE_NAME --query 'Role.Arn' --output text)
 
-aws lambda create-function \
+LAMBDA_FUNCTION_ARN=$(aws lambda create-function \
     --function-name $LAMBDA_FUNCTION_NAME \
     --zip-file fileb://function.zip \
     --handler lambda_function.lambda_handler \
     --runtime python3.9 \
     --role $LAMBDA_ROLE_ARN \
-    --environment Variables="{LAMBDA_FUNCTION_2_NAME=$LAMBDA_FUNCTION_2_NAME}"
+    --environment Variables="{LAMBDA_FUNCTION_2_NAME=$LAMBDA_FUNCTION_2_NAME}")
 
